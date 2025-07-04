4598f987b24533004ea966d3f769696943c82eee
zhangyi <zhangyi1101@foxmail.com>
2025-07-04 12:05:13 +0800

    yi - update dependencies; write run_mcp script to add mcp server in cursor

---
+++ run_mcp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Users/zhangyi/workspace/learning/llm/aibootcamp
+poetry run python /Users/zhangyi/workspace/learning/llm/aibootcamp/src/aibootcamp/mcp/sqlite_mcp/server.py --server_type stdio
